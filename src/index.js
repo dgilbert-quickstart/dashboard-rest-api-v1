d324347a48b619bea53dc8243ad715b0ed0d9909
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-06 17:37:56 +0000

    update react dashboard app 1.0.1

--- src/index.js
+++ src/index.js
@@ -9,16 +9,38 @@ const SERVER_PORT = 8080; //localhost range - 3000 - 9999
 
 //- GET endpoint - url endpoint or route - ie: json placeholder, githpy api 
 app.get("/", (req,res) => {
-    const _msg = "# Dashboard api 1.0.0";
+    const _msg = "# Dashboard api 1.0.1";
     console.log(_msg)
     res.send(_msg)
 });
 
 app.get("/test1", (req,res) => {
-    const _msg = "# Dashboard api 1.0.0 - test1 route";
+    const _msg = "# Dashboard api 1.0.1 - test1 route";
     console.log(_msg)
     res.send(_msg)
 });
+
+ app.get("/login/:username/:password",(req, res) =>{
+ 
+     const _username = req.params.username;
+     const _password = req.params.password;
+ 
+     let _msg = `login route, username: ${_username}, password: ${_password}`;
+     console.log(_msg)
+ 
+     let _data = {};
+ 
+     _msg = "* login successful";
+     _data = {'msg':_msg, 'login':true};
+ 
+     if (_username === null || _username === undefined || _username.trim().toLowerCase() !== "admin")
+     {
+         _msg = "* invalid username/password";
+         _data = {'msg':_msg, 'login':false};
+     }
+  
+     res.send(_data)
+ });
 
 //-other - POST, DELETE, PUT
 
